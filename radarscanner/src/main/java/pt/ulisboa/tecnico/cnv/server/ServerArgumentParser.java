ffbc3c859c5c10c480762b3f01a34b2a30b3a8e5
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-04-18 17:49:33 +0100

    Add existing server code
    Co-authored-by: João David <joaodavid@tecnico.ulisboa.pt>

---
+++ radarscanner/src/main/java/pt/ulisboa/tecnico/cnv/server/ServerArgumentParser.java
@@ -0,0 +1,186 @@
+package pt.ulisboa.tecnico.cnv.server;
+
+import org.apache.commons.cli.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerArgumentParser {
+    public enum ServerParameters {
+        /**
+         * Set debug mode.
+         */
+        DEBUG_SHORT("d"), DEBUG("debug"), OUTPUT_DIR_SHORT("o"), OUTPUT_DIR("output-directory"), ADDRESS("address"),
+        PORT("port"), MAPS_DIR("maps");
+
+        private final String text;
+
+        ServerParameters(final String text) {
+            this.text = text;
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see java.lang.Enum#toString()
+         */
+        @Override
+        public String toString() {
+            return this.text;
+        }
+    }
+
+    protected final Options options = new Options();
+    protected final Map<String, Object> argValues = new HashMap<>();
+
+    protected final CommandLineParser parser = new DefaultParser();
+    protected final HelpFormatter formatter = new HelpFormatter();
+    protected CommandLine cmd = null;
+
+    private void parseValues() {
+
+        // Process output directory argument.
+        String outDirPath = System.getProperty("java.io.tmpdir");
+        if (this.cmd.hasOption(ServerParameters.OUTPUT_DIR.toString())) {
+            outDirPath = this.cmd.getOptionValue(ServerParameters.OUTPUT_DIR.toString()).replace("'", "");
+            File outDirHandle = new File(outDirPath).getAbsoluteFile();
+            if (!outDirHandle.exists()) {
+                boolean result = false;
+                try {
+                    result = outDirHandle.mkdir();
+                } catch (SecurityException se) {
+                    System.out.println("Error creating output directory:" + outDirPath);
+                    se.printStackTrace();
+                    System.exit(1);
+                }
+                if (result) {
+                    System.out.println("Created output directory:\t" + outDirPath);
+                } else if (outDirHandle.exists()) {
+                    System.out.println("Output directory already existed:\t" + outDirPath);
+                } else {
+                    System.out.println("Create directory failed:\t " + outDirPath);
+                    System.exit(1);
+                }
+                this.argValues.put(ServerParameters.OUTPUT_DIR.toString(), outDirPath);
+            } else if (!outDirHandle.isDirectory()) {
+                System.out.println("The given output directory path was a file but should be a directory:");
+                System.out.println(outDirPath);
+                System.out.println("Exiting.");
+                System.exit(1);
+            } else {
+                this.argValues.put(ServerParameters.OUTPUT_DIR.toString(), outDirPath);
+            }
+        } else {
+            final String tempDir = System.getProperty("java.io.tmpdir");
+            this.argValues.put(ServerParameters.OUTPUT_DIR.toString(), tempDir);
+        }
+
+        if (this.cmd.hasOption(ServerParameters.ADDRESS.toString())) {
+            final String address = this.cmd.getOptionValue(ServerParameters.ADDRESS.toString());
+            this.argValues.put(ServerParameters.ADDRESS.toString(), address);
+        } else {
+            this.argValues.put(ServerParameters.ADDRESS.toString(), "127.0.0.1");
+        }
+
+        if (this.cmd.hasOption(ServerParameters.PORT.toString())) {
+            final String port = this.cmd.getOptionValue(ServerParameters.PORT.toString());
+            this.argValues.put(ServerParameters.PORT.toString(), new Integer(port));
+        } else {
+            this.argValues.put(ServerParameters.PORT.toString(), new Integer(8000));
+        }
+
+        if (this.cmd.hasOption(ServerParameters.MAPS_DIR.toString())) {
+            String mapsDirectory = this.cmd.getOptionValue(ServerParameters.MAPS_DIR.toString());
+
+            if (mapsDirectory.endsWith("/") || mapsDirectory.endsWith("\\")) {
+                mapsDirectory = mapsDirectory.substring(0, mapsDirectory.length() - 1);
+            }
+
+            this.argValues.put(ServerParameters.MAPS_DIR.toString(), mapsDirectory);
+        } else {
+            this.argValues.put(ServerParameters.MAPS_DIR.toString(), "datasets");
+        }
+
+        this.argValues.put(ServerParameters.DEBUG.toString(), cmd.hasOption(ServerParameters.DEBUG.toString()));
+        if (this.cmd.hasOption(ServerParameters.DEBUG.toString())) {
+            for (Map.Entry<String, Object> param : this.argValues.entrySet()) {
+                System.out.println(param.getKey() + "\t" + param.getValue().toString());
+            }
+            System.out.println("\n");
+        }
+
+    }
+
+    private void setupCLIOptions() {
+
+        final Option addressOption = new Option(ServerParameters.ADDRESS.toString(), true,
+                "server listen address (default: 127.0.0.1).");
+        addressOption.setRequired(false);
+        this.options.addOption(addressOption);
+
+        final Option portOption = new Option(ServerParameters.PORT.toString(), true,
+                "server listen port (default: 8000).");
+        portOption.setRequired(false);
+        this.options.addOption(portOption);
+
+        final Option mapsDirectoryOption = new Option(ServerParameters.MAPS_DIR.toString(), true,
+                "directory with maps on the server (default: WORKING_DIR/datasets).");
+        mapsDirectoryOption.setRequired(false);
+        this.options.addOption(mapsDirectoryOption);
+
+        final Option outputDirOption = new Option(ServerParameters.OUTPUT_DIR_SHORT.toString(),
+                ServerParameters.OUTPUT_DIR.toString(), true,
+                "output directory for generated images. By omission it is the system's temp directory.");
+        outputDirOption.setRequired(false);
+        this.options.addOption(outputDirOption);
+
+        final Option debugOption = new Option(ServerParameters.DEBUG_SHORT.toString(),
+                ServerParameters.DEBUG.toString(), false, "set debug mode.");
+        debugOption.setRequired(false);
+        this.options.addOption(debugOption);
+    }
+
+    public ServerArgumentParser(final String[] args) {
+
+        // Prepare program-specific options provided in a subclass of this one.
+        this.setupCLIOptions();
+
+        System.out.println("> [ServerArgumentParser]: setupCLIOptions() DONE.");
+
+        // Set values
+        try {
+            this.cmd = this.parser.parse(this.options, args);
+        } catch (ParseException e) {
+            System.out.println("> [ServerArgumentParser]: ParseException.");
+            System.out.println(e.getMessage());
+            this.formatter.printHelp("utility-name", this.options);
+            System.exit(1);
+        }
+
+        // Parse program-specific arguments provided in a subclass of this one.
+        this.parseValues();
+
+    }
+
+    public String getServerAddress() {
+        return (String) this.argValues.get(ServerParameters.ADDRESS.toString());
+    }
+
+    public Integer getServerPort() {
+        return (Integer) this.argValues.get(ServerParameters.PORT.toString());
+    }
+
+    public String getMapsDirectory() {
+        return (String) this.argValues.get(ServerParameters.MAPS_DIR.toString());
+    }
+
+    public Boolean isDebugging() {
+        return (Boolean) this.argValues.get(ServerParameters.DEBUG.toString());
+    }
+
+    public String getOutputDirectory() {
+        return (String) this.argValues.get(ServerParameters.OUTPUT_DIR.toString());
+    }
+
+}
---
+++ radarscanner/src/main/java/pt/ulisboa/tecnico/cnv/server/WebServer.java
@@ -0,0 +1,166 @@
+package pt.ulisboa.tecnico.cnv.server;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import pt.ulisboa.tecnico.cnv.solver.Solver;
+import pt.ulisboa.tecnico.cnv.solver.SolverFactory;
+
+import javax.imageio.ImageIO;
+
+public class WebServer {
+
+    static ServerArgumentParser sap = null;
+
+    public static void main(final String[] args) throws Exception {
+
+        try {
+            // Get user-provided flags.
+            WebServer.sap = new ServerArgumentParser(args);
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+        System.out.println("> Finished parsing Server args.");
+
+        // final HttpServer server = HttpServer.create(new
+        // InetSocketAddress("127.0.0.1", 8000), 0);
+
+        final HttpServer server = HttpServer
+                .create(new InetSocketAddress(WebServer.sap.getServerAddress(), WebServer.sap.getServerPort()), 0);
+
+        server.createContext("/scan", new MyHandler());
+
+        // be aware! infinite pool of threads!
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
+
+        System.out.println(server.getAddress().toString());
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(final HttpExchange t) throws IOException {
+
+            // Get the query.
+            final String query = t.getRequestURI().getQuery();
+
+            System.out.println("> Query:\t" + query);
+
+            // Break it down into String[].
+            final String[] params = query.split("&");
+
+            /*
+             * for(String p: params) { System.out.println(p); }
+             */
+
+            // Store as if it was a direct call to SolverMain.
+            final ArrayList<String> newArgs = new ArrayList<>();
+            for (final String p : params) {
+                final String[] splitParam = p.split("=");
+
+                if (splitParam[0].equals("i")) {
+                    splitParam[1] = WebServer.sap.getMapsDirectory() + "/" + splitParam[1];
+                }
+
+                newArgs.add("-" + splitParam[0]);
+                newArgs.add(splitParam[1]);
+
+                /*
+                 * System.out.println("splitParam[0]: " + splitParam[0]);
+                 * System.out.println("splitParam[1]: " + splitParam[1]);
+                 */
+            }
+
+            if (sap.isDebugging()) {
+                newArgs.add("-d");
+            }
+
+            // Store from ArrayList into regular String[].
+            final String[] args = new String[newArgs.size()];
+            int i = 0;
+            for (String arg : newArgs) {
+                args[i] = arg;
+                i++;
+            }
+
+            /*
+             * for(String ar : args) { System.out.println("ar: " + ar); }
+             */
+
+            // Create solver instance from factory.
+            final Solver s = SolverFactory.getInstance().makeSolver(args);
+
+            if (s == null) {
+                System.out.println("> Problem creating Solver. Exiting.");
+                System.exit(1);
+            }
+
+            // Write figure file to disk.
+            File responseFile = null;
+            try {
+
+                final BufferedImage outputImg = s.solveImage();
+
+                final String outPath = WebServer.sap.getOutputDirectory();
+
+                final String imageName = s.toString();
+
+                /*
+                 * if(ap.isDebugging()) { System.out.println("> Image name: " + imageName); }
+                 */
+
+                final Path imagePathPNG = Paths.get(outPath, imageName);
+                ImageIO.write(outputImg, "png", imagePathPNG.toFile());
+
+                responseFile = imagePathPNG.toFile();
+
+            } catch (final FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (final IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Send response to browser.
+            final Headers hdrs = t.getResponseHeaders();
+
+            hdrs.add("Content-Type", "image/png");
+
+            hdrs.add("Access-Control-Allow-Origin", "*");
+            hdrs.add("Access-Control-Allow-Credentials", "true");
+            hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+            hdrs.add("Access-Control-Allow-Headers",
+                    "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+            t.sendResponseHeaders(200, responseFile.length());
+
+            final OutputStream os = t.getResponseBody();
+            Files.copy(responseFile.toPath(), os);
+
+            os.close();
+
+            System.out.println("> Sent response to " + t.getRemoteAddress().toString());
+        }
+    }
+
+}

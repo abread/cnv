9bb9289609c10803d1088153acae999065a7fe91
joaocmd <joaodavid1999@hotmail.com>
2021-05-31 15:00:23 +0100

    Javadoc to instrumenter

--- wsinstrumenter/src/main/java/cnv/wsinstrumenter/MultiThreadedStatistics.java
+++ wsinstrumenter/src/main/java/cnv/wsinstrumenter/MultiThreadedStatistics.java
@@ -36,13 +36,19 @@ public class MultiThreadedStatistics {
         System.exit(-1);
     }
 
+    /**
+     * Calls the instrumenter recursively on all .class files in a directory and its subdirectories.
+     * @param in_dir the input directory of classes to instrument
+     * @param out_dir the output directory
+     * @throws IOException if the directories can't be created
+     */
     public static void instrumentDirRecurse(Path in_dir, Path out_dir) throws IOException {
         if (!Files.isDirectory(in_dir)) {
-            System.err.println(String.format("%s is not a directory", in_dir.toString()));
+            System.err.printf("%s is not a directory%n", in_dir);
             System.exit(-1);
         } else if (!Files.isDirectory(out_dir)) {
             if (Files.exists(out_dir)) {
-                System.err.println(String.format("%s is not a directory", out_dir.toString()));
+                System.err.printf("%s is not a directory%n", out_dir);
                 System.exit(-1);
             } else {
                 Files.createDirectories(out_dir);
@@ -60,11 +66,16 @@ public class MultiThreadedStatistics {
             } else if (in_path.toString().endsWith(".class")) {
                 instrumentFile(in_path, out_path);
             } else {
-                System.err.println(String.format("File not processed: %s", in_path.toString()));
+                System.err.printf("File not processed: %s%n", in_path);
             }
         }
     }
 
+    /**
+     * Runs the MethodCallTracker.instrument on the given .class file.
+     * @param in_path the file to instrument
+     * @param out_path the corresponding output file
+     */
     public static void instrumentFile(Path in_path, Path out_path) {
         ClassInfo ci = new ClassInfo(in_path.toString());
 
@@ -83,6 +94,10 @@ public class MultiThreadedStatistics {
         instrumentDirRecurse(in_dir, out_dir);
     }
 
+    /**
+     * This class contains the instrumenter logic. It adds a method call to
+     * CLASS_METRIC_TRACKER.METHOD_INCR_METHOD_COUNT before each bytecode method.
+     */
     private static class MethodCallTracker {
         private static final String METHOD_INCR_METHOD_COUNT = "incrMethodCount";
 
